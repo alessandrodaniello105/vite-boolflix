0ac2efbd99caccd81de00b33abf9fbc8d22f4427
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-16 15:53:59 +0200

    [fix]: text input search empty

--- src/data/store.js
+++ src/data/store.js
@@ -4,6 +4,6 @@ export const store = reactive({
   movieapiURL: 'https://api.themoviedb.org/3/search/movie?api_key=878007cbe1145f59af923c1e32e787a7',
   moviesList: [],
   tvsList: [],
-  textToSearch: null,
+  textToSearch: "",
   totalResults: null
 })
