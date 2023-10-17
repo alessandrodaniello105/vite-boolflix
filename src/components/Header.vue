27e208ebbbb77105a7d223962bc9a6630e7bd165
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-17 11:32:29 +0200

    [add]: logo+lang flags(en-it);[edit]: header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,10 @@ export default {
 <template>
   <header>
 
+    <div class="logo">
+      <img src="/logo-boolflix.png" alt="">
+    </div>
+
     <SearchBar />
 
   </header>
@@ -24,7 +28,18 @@ export default {
 header {
   color: $font-color;
   background-color: black;
-  min-height: $h-height;
+  height: $h-height;
+  .logo {
+    width: 300px;
+    height: 100%;
+    display: flex;
+    float: left;
+    img {
+      height: 60%;
+      margin-left: 10px;
+      align-self: center;
+    }
+  }
 }
 
 </style>
