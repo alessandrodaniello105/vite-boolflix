93cdc0aafda67a04d1a18fc012317d9cc1a932d5
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-18 11:37:17 +0200

    [del]: axios import from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script>
-import axios from 'axios'
 import Main from './components/Main.vue';
 import Header from './components/Header.vue';
 import {store} from './data/store.js'
@@ -30,7 +29,6 @@ export default {
     :tvsList="store.tv"
     :isNotFound="store.totalResults == 0"
   />
-
 
 </template>
 
